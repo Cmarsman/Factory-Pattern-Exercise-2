bdab0e61f099cfe31a52d6fdccf2802b845eac76
Cmarsman <chris_marsden@yahoo.com>
2024-05-16 14:12:53 -0500

    exercise 2

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace FactoryPatternExercise2
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FactoryPatternExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What type of database do you want to use? [ List, SQL, or Mongo?]");
+            string userInput = Console.ReadLine();
+
+            IDataAccess instance = DataAccessFactory.GetDataAccessType(userInput);
+            Console.WriteLine($"Thank you for choosing {userInput}");
         }
     }
 }
